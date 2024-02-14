0fbe6609ae7381c25f042e6e920ebf5960220bf8
Fredrik Sundberg <ted.sundberg@gmail.com>
2024-02-14 06:10:01 +0100

    feat: Use social icons in EmployeeCard

--- src/components/EmployeeCard/EmployeeCard.tsx
+++ src/components/EmployeeCard/EmployeeCard.tsx
@@ -1,8 +1,75 @@
+import React from "react";
 import type { Employee } from "../../types/Employee";
 import "./EmployeeCard.css";
 
-export const EmployeeCard = ({
-  employee: {
+type SocialIconProps = {
+  type: "gitHub" | "linkedIn" | "stackOverflow" | "twitter";
+  href: string;
+  alt: string;
+};
+
+const SocialIcon: React.FC<SocialIconProps> = ({ type, href, alt }) => (
+  <a href={href}>
+    <img
+      src={`${type.toLowerCase()}.svg`}
+      alt={`${alt} on ${type}`}
+      className="icon"
+    />
+  </a>
+);
+
+type SocialProps = Pick<
+  Employee,
+  "name" | "gitHub" | "linkedIn" | "stackOverflow" | "twitter"
+>;
+
+const EmployeeSocial: React.FC<SocialProps> = ({
+  name,
+  gitHub,
+  twitter,
+  stackOverflow,
+  linkedIn,
+}) => {
+  return (
+    <div className="social">
+      {gitHub && (
+        <SocialIcon
+          type="gitHub"
+          href={`https://github.com/${gitHub}`}
+          alt={name}
+        />
+      )}
+      {linkedIn && (
+        <SocialIcon
+          type="linkedIn"
+          href={`https://www.linkedin.com${linkedIn}`}
+          alt={name}
+        />
+      )}
+      {twitter && (
+        <SocialIcon
+          type="twitter"
+          href={`https://twitter.com/${twitter}`}
+          alt={name}
+        />
+      )}
+      {stackOverflow && (
+        <SocialIcon
+          type="stackOverflow"
+          href={`https://stackoverflow.com/users/${stackOverflow}`}
+          alt={name}
+        />
+      )}
+    </div>
+  );
+};
+
+type EmployeeCardProps = {
+  employee: Employee;
+};
+
+export const EmployeeCard: React.FC<EmployeeCardProps> = ({ employee }) => {
+  const {
     name,
     imagePortraitUrl,
     office,
@@ -10,28 +77,14 @@ export const EmployeeCard = ({
     twitter,
     stackOverflow,
     linkedIn,
-  },
-}: {
-  employee: Employee;
-}) => {
+  } = employee;
+
   return (
     <div className="employeeCard">
       <img src={imagePortraitUrl || "no-image.jpg"} alt={name} />
       <h2>{name}</h2>
       <p>Office: {office}</p>
-
-      <div className="social">
-        {gitHub && <a href={`https://github.com/${gitHub}`}>GitHub</a>}
-        {twitter && <a href={`https://twitter.com/${twitter}`}>Twitter</a>}
-        {stackOverflow && (
-          <a href={`https://stackoverflow.com/users/${stackOverflow}`}>
-            Stack Overflow
-          </a>
-        )}
-        {linkedIn && (
-          <a href={`https://www.linkedin.com/${linkedIn}`}>LinkedIn</a>
-        )}
-      </div>
+      <EmployeeSocial {...{ name, gitHub, twitter, stackOverflow, linkedIn }} />
     </div>
   );
 };
